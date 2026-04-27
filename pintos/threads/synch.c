21085984e46e05cafb1ec500e5937a9da182745b
Y-duchi <du822623@gmail.com>
2026-04-27 17:36:41 +0900

    condvar머지

--- pintos/threads/synch.c
+++ pintos/threads/synch.c
@@ -42,6 +42,7 @@
      하나를 깨운다. */
 
 static bool wake_up_less(const struct list_elem *, const struct list_elem *, void *aux);
+static bool cmp_sema_priority(const struct list_elem *a, const struct list_elem *b, void *aux UNUSED);
 
 void
 sema_init (struct semaphore *sema, unsigned value) {
@@ -66,7 +67,7 @@ sema_down (struct semaphore *sema) {
 
 	old_level = intr_disable ();
 	while (sema->value == 0) {
-		list_insert_ordered(&sema->waiters, &thread_current()->elem, cmp_priority, NULL);
+		list_insert_ordered(&sema->waiters, &thread_current()->elem, cmp_sema_priority, NULL);
 		thread_block ();
 	}
 	sema->value--;
@@ -245,6 +246,7 @@ lock_held_by_current_thread (const struct lock *lock) {
 struct semaphore_elem {
 	struct list_elem elem;              /* 리스트 원소. */
 	struct semaphore semaphore;         /* 해당 세마포어. */
+	int priority;
 };
 
 /* 조건 변수 `COND`를 초기화한다. 조건 변수는 한 코드 조각이
@@ -256,25 +258,35 @@ cond_init (struct condition *cond) {
 
 	list_init (&cond->waiters);
 }
+/* cond 정렬 비교함수
+*/
+bool cmp_sema_priority(const struct list_elem *a, const struct list_elem *b, void *aux UNUSED) {
+	struct semaphore_elem *sa = list_entry(a, struct semaphore_elem, elem);
+	struct semaphore_elem *sb = list_entry(b, struct semaphore_elem, elem);
 
-/* `LOCK`를 원자적으로 해제하고, 다른 코드가 `COND`를 신호할 때까지
-   기다린다. `COND`가 신호되면 반환하기 전에 `LOCK`를 다시 획득한다.
-   이 함수를 호출하기 전에는 반드시 `LOCK`를 보유하고 있어야 한다.
+	return sa->priority > sb->priority;
 
-   이 함수가 구현하는 모니터는 "Hoare" 스타일이 아니라 "Mesa"
-   스타일이다. 즉, 신호를 보내는 것과 받는 것은 원자적 연산이 아니다.
-   따라서 보통은 대기가 끝난 뒤 조건을 다시 확인하고, 필요하면
-   다시 기다려야 한다.
+}
 
-   하나의 조건 변수는 하나의 락에만 연결되지만, 하나의 락은
-   여러 조건 변수와 연결될 수 있다. 즉 락에서 조건 변수로는
-   일대다 관계다.
+	/* `LOCK`를 원자적으로 해제하고, 다른 코드가 `COND`를 신호할 때까지
+	   기다린다. `COND`가 신호되면 반환하기 전에 `LOCK`를 다시 획득한다.
+	   이 함수를 호출하기 전에는 반드시 `LOCK`를 보유하고 있어야 한다.
 
-   이 함수는 잠들 수 있으므로 인터럽트 핸들러 안에서 호출하면
-   안 된다. 인터럽트를 끈 상태에서 호출할 수는 있지만, 잠들어야
-   하면 인터럽트가 다시 켜진다. */
+	   이 함수가 구현하는 모니터는 "Hoare" 스타일이 아니라 "Mesa"
+	   스타일이다. 즉, 신호를 보내는 것과 받는 것은 원자적 연산이 아니다.
+	   따라서 보통은 대기가 끝난 뒤 조건을 다시 확인하고, 필요하면
+	   다시 기다려야 한다.
+
+	   하나의 조건 변수는 하나의 락에만 연결되지만, 하나의 락은
+	   여러 조건 변수와 연결될 수 있다. 즉 락에서 조건 변수로는
+	   일대다 관계다.
+
+	   이 함수는 잠들 수 있으므로 인터럽트 핸들러 안에서 호출하면
+	   안 된다. 인터럽트를 끈 상태에서 호출할 수는 있지만, 잠들어야
+	   하면 인터럽트가 다시 켜진다. */
 void
-cond_wait (struct condition *cond, struct lock *lock) {
+cond_wait(struct condition *cond, struct lock *lock)
+{
 	struct semaphore_elem waiter;
 
 	ASSERT (cond != NULL);
@@ -282,11 +294,12 @@ cond_wait (struct condition *cond, struct lock *lock) {
 	ASSERT (!intr_context ());
 	ASSERT (lock_held_by_current_thread (lock));
 
-	sema_init (&waiter.semaphore, 0);
-	list_push_back (&cond->waiters, &waiter.elem);
-	lock_release (lock);
+	sema_init(&waiter.semaphore, 0);
+	waiter.priority = thread_current()->priority;
+	list_insert_ordered(&cond->waiters, &waiter.elem, cmp_sema_priority, NULL);
+	lock_release(lock); // sema_down으로 잠들기 전에 락을 풀어줘야 다른 스레드가 락을 잡아 실행할 수 있음?
 	sema_down (&waiter.semaphore);
-	lock_acquire (lock);
+	lock_acquire(lock); // signal을 받고 깨어나면 wait()가 리턴하기 전에 lock을 다시 잡는다.
 }
 
 /* `COND`에서 기다리는 스레드가 하나라도 있으면,
@@ -302,9 +315,8 @@ cond_signal (struct condition *cond, struct lock *lock UNUSED) {
 	ASSERT (!intr_context ());
 	ASSERT (lock_held_by_current_thread (lock));
 
-	if (!list_empty (&cond->waiters))
-		sema_up (&list_entry (list_pop_front (&cond->waiters),
-					struct semaphore_elem, elem)->semaphore);
+	if (!list_empty(&cond->waiters))
+		sema_up(&list_entry(list_pop_front(&cond->waiters), struct semaphore_elem, elem)->semaphore);
 }
 
 /* `COND`에서 기다리는 모든 스레드를 깨운다.
