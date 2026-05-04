e0274e3820a3a20fc6a8085604fd469342f68170
Y-duchi <du822623@gmail.com>
2026-05-04 13:45:44 +0900

    process_wait

--- pintos/userprog/process.c
+++ pintos/userprog/process.c
@@ -202,10 +202,20 @@ process_exec (void *f_name) {
  * This function will be implemented in problem 2-2.  For now, it
  * does nothing. */
 int
-process_wait (tid_t child_tid UNUSED) {
+process_wait (tid_t child_tid) {
 	/* XXX: Hint) The pintos exit if process_wait (initd), we recommend you
 	 * XXX:       to add infinite loop here before
 	 * XXX:       implementing the process_wait. */
+	struct thread *cur = thread_current();
+	struct list_elem *elem;
+	for (elem = list_begin(&cur->children); elem != list_end(&cur->children); elem = list_next(elem)) {
+		struct thread *child = list_entry(elem, struct thread, child_elem);
+		if (child->tid == child_tid) {
+			sema_down(&child->wait_sema);
+			int exit_save = child->exit_status;
+			return exit_save;
+		}
+	}
 	return -1;
 }
 
@@ -217,7 +227,7 @@ process_exit (void) {
 	 * TODO: Implement process termination message (see
 	 * TODO: project2/process_termination.html).
 	 * TODO: We recommend you to implement process resource cleanup here. */
-
+	sema_up(&curr->wait_sema);
 	process_cleanup ();
 }
 
@@ -462,6 +472,8 @@ load (char *file_name, struct intr_frame *if_) {
 
 	if_->R.rdi = argc;
 	if_->R.rsi = argv_start;
+
+	hex_dump(if_->rsp, (void *)if_->rsp, USER_STACK - if_->rsp, true);
 
 	success = true;
 done:
